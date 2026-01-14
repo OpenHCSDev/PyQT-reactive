b99561e120a36932eaad2d121468d9ce2081e61e
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-01-14 16:20:55 -0500

    Add generic preview formatter helpers

--- src/pyqt_formgen/utils/__init__.py
+++ src/pyqt_formgen/utils/__init__.py
@@ -2,4 +2,9 @@
 Utility helpers for PyQt FormGen.
 """
 
-__all__ = []
+from .preview_formatters import check_enabled_field, format_preview_value
+
+__all__ = [
+    "check_enabled_field",
+    "format_preview_value",
+]
